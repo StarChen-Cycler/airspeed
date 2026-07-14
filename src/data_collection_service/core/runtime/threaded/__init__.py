ec79f71ac3d06478812aae506792d02cda3fe2e4
StarChen-Cycler <1976693250@qq.com>
2026-07-14 15:19:01 +0800

    refactor(data-collection): actually stage deleted threaded writer files
    Follow-up to 7c9134a: the previous commit message described the deletion
    but the deletions were not staged. This commit removes the dead threaded
    writer files from the index.

--- src/data_collection_service/core/runtime/threaded/__init__.py
+++
@@ -1,2 +0,0 @@
-from core.runtime.threaded.stream_tracker import StreamTracker, StreamMetrics, StreamStatus
-__all__ = ["StreamTracker", "StreamMetrics", "StreamStatus"]
--- src/data_collection_service/core/storage/threaded/__init__.py
+++
@@ -1,2 +0,0 @@
-from core.storage.threaded.airs_hdf5_writer import AirsHdf5Writer, AirsHdf5WriterError
-__all__ = ["AirsHdf5Writer", "AirsHdf5WriterError"]
